227283d19f816bf8a031a2d927128ccaa3e4e957
nehagujar <coolsnow02@gmail.com>
2019-06-03 13:16:02 +0530

    renamed s3 client obj

--- spec/store_spec.rb
+++ spec/store_spec.rb
@@ -1,22 +1,22 @@
 RSpec.describe Store do
-  let (:s3) { Aws::S3::Client.new(stub_responses: true) }
+  let (:aws_s3_client) { Aws::S3::Client.new(stub_responses: true) }
   let (:s3_store) { Store.new }
   
   before do
-    allow(s3_store).to receive(:aws_client).and_return(s3)
+    allow(s3_store).to receive(:aws_client).and_return(aws_s3_client)
   end
   
   context '#list_buckets' do
     
     it 'should list all the buckets in the linked account when response is not empty/nil' do
-      s3.stub_responses(:list_buckets, buckets: [{ name: 'bucket1' }, { name: 'bucket2' }, { name: 'bucket3' }])
-      response = s3.list_buckets
+      aws_s3_client.stub_responses(:list_buckets, buckets: [{ name: 'bucket1' }, { name: 'bucket2' }, { name: 'bucket3' }])
+      response = aws_s3_client.list_buckets
       expect(s3_store).to receive(:list_buckets).and_return(response)
       s3_store.list_buckets
     end
     
     it 'display message when there are no buckets - empty / nil response' do
-      s3.stub_responses(:list_buckets, buckets: [])
+      aws_s3_client.stub_responses(:list_buckets, buckets: [])
       expect { s3_store.list_buckets }.to raise_error(S3StoreEmptyError)
     end
   end
@@ -28,13 +28,11 @@ RSpec.describe Store do
     let (:response) { double('response') }
     
     before do
-      s3.stub_responses(:create_bucket)
+      aws_s3_client.stub_responses(:create_bucket)
     end
 
     it 'should create a new bucket with bucket name' do
       expect(s3_store).to receive(:create_bucket).with(bucket_name)
-      expect(s3).to receive(:create_bucket).with({bucket: bucket_name}).and_return(response)
-      s3.create_bucket({bucket: bucket_name})
       s3_store.create_bucket(bucket_name)
     end
 
